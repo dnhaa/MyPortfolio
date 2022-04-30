6f8b2e3cc5a2464a06f4951f8ae11a5b5edb3909
dnhaa <elnganha@gmail.com>
2022-04-30 22:23:29 +0700

    wip: about, contact

--- js/script.js
+++ js/script.js
@@ -1,17 +1,90 @@
-function skillsimg() {
-  var reveals = document.querySelectorAll(".skillsimg");
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+const fdowns = document.querySelectorAll('.fdown');
 
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
+
+
+observer = new IntersectionObserver((entries) =>{
+  
+  entries.forEach(entry => {
+  if(entry.intersectionRatio > 0){
+    entry.target.style.animation = `float-down 1.7s forwards ease-in-out`;
+     // ${entry.target.dataset.delay}
   }
-}
+  else {
+     entry.target.style.animation = 'none';
+  }
+  })
 
-window.addEventListener("scroll", skillsimg);
+})
+// observer.observe(imgs);
+fdowns.forEach(fdown => {
+  observer.observe(fdown)
+})
+
+const fups = document.querySelectorAll('.fup');
+observer = new IntersectionObserver((entries) =>{
+  
+  entries.forEach(entry => {
+  if(entry.intersectionRatio > 0){
+    entry.target.style.animation = `float-up 1s forwards ease-in-out`;
+  }
+  else {
+     entry.target.style.animation = 'none';
+  }
+  })
+
+})
+fups.forEach(fup => {
+  observer.observe(fup)
+})
+
+
+const flefts = document.querySelectorAll('.fleft');
+observer = new IntersectionObserver((entries) =>{
+  
+  entries.forEach(entry => {
+  if(entry.intersectionRatio > 0){
+    entry.target.style.animation = `float-left 1s forwards ease-in-out`;
+  }
+  else {
+     entry.target.style.animation = 'none';
+  }
+  })
+
+})
+flefts.forEach(fleft => {
+  observer.observe(fleft)
+})
+
+const frights = document.querySelectorAll('.fright');
+observer = new IntersectionObserver((entries) =>{
+  
+  entries.forEach(entry => {
+  if(entry.intersectionRatio > 0){
+    entry.target.style.animation = `float-right 1s forwards ease-in-out`;
+  }
+  else {
+     entry.target.style.animation = 'none';
+  }
+  })
+
+})
+frights.forEach(fright => {
+  observer.observe(fright)
+})
+
+
+let openBtn = document.querySelector(".openSideNav");
+openBtn.addEventListener("click", () => {
+  showNav();
+});
+let closeBtn = document.querySelector(".closeBtn");
+closeBtn.addEventListener("click", () => {
+  hideNav();
+});
+function showNav(){
+  document.querySelector("#about").style.opacity = "100%";
+}
+function hideNav(){
+  document.querySelector("#about").style.opacity = "0";
+}
